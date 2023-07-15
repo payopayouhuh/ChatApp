d63ad469a777c3e556f3f30184020cf9e7d0108e
payopayouhuh <134220954+payopayouhuh@users.noreply.github.com>
2023-07-15 11:29:39 +0900

    chat

---
+++ src/TweetIndex.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+
+class TweetIndex extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.bottomRef = React.createRef();  // 最下部への参照を作成
+      }
+    
+      componentDidUpdate() {
+        this.scrollToBottom();  // 更新がある度にスクロール
+      }
+    
+      scrollToBottom() {
+        this.bottomRef.current.scrollIntoView({ behavior: "smooth" });
+      }    
+
+    render() {
+      return (
+        <React.Fragment>
+          <ul>
+            {this.props.items.map((item, i) => (
+              <li key={i}>
+              <div className={`tweet-bc ${item.user === 'You' ? 'you-tweet' : ''}`}>
+                <div className="balloon6">
+                  <div className="faceicon">
+                    <img src={item.user === 'You' ? 'you_icon.png' : 'icon.png'}/>
+                    {item.user}
+                  </div>
+                  <div className="tweeting">
+                    <div className="tweets">
+                      <p>{item.text}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </li>
+            ))}
+            <div ref={this.bottomRef}></div>
+          </ul>
+        </React.Fragment>
+      );
+    }
+  }
+  
+  export default TweetIndex;
+  
